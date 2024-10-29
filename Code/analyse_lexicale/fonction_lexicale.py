0fcdcc7b45bc25c92c37ed6cc770e64ad750cf55
lina <lina@LAPTOP-GA52FUVE.localdomain>
2024-10-29 16:12:50 +0100

    classe token 2.0 et modification de la fonction lexicale

--- Code/analyse_lexicale/fonction_lexicale.py
+++ Code/analyse_lexicale/fonction_lexicale.py
@@ -1,258 +1,141 @@
+import re
+from token import TokenType, BaseToken, KeywordToken, OperatorToken, LiteralToken, IdentifierToken, PunctuationToken, NewlineToken, OperatorUnaryToken, OperatorBinaryToken
+
 
 def lire_fichier(source):
     with open(source, 'r') as fichier:
         contenu = fichier.read()
     return contenu
 
+
 class Lexeur:
-    def __init__(self,contenu):
+    def __init__(self, contenu):
         self.contenu = contenu
         self.position = -1
-        self.curseur_position = 0
         self.ligne_position = 1
+        self.curseur_position = 0
         self.charactere_actuelle = None
         self.token = None
-        self.token_nombre =False
+        self.token_nombre = False
         self.fin_fichier = False
-        self.pile = [0]
-    
+
     def lire(self):
-        if(self.curseur_position >= len(self.contenu)):
+        if self.curseur_position >= len(self.contenu):
             self.fin_fichier = True
         else:
-            self.position +=1
+            self.position += 1
             self.curseur_position += 1
             self.charactere_actuelle = self.contenu[self.position]
-            if(self.charactere_actuelle == '\n'):
-                self.ligne_position +=1
+            if self.charactere_actuelle == '\n':
+                self.ligne_position += 1
 
-    def retour(self):
-            if (self.curseur_position==0):
-                pass
-            else: 
-                self.position -=1
-                self.curseur_position -=1
-                self.charactere_actuelle = self.contenu[self.position]
-                if (self.charactere_actuelle == '\n'):
-                    self.ligne_position = 1
+    def peek(self):
+        if self.curseur_position + 1 < len(self.contenu):
+            return self.contenu[self.curseur_position + 1]
+        return None
 
     def chiffre(self):
-        if self.charactere_actuelle.isdigit():  
-            return True
-        else:
-            return False
-    
+        return self.charactere_actuelle.isdigit()
+
     def charactere(self):
-        alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        if(self.charactere_actuelle in alphabet):
-            return True
-        else:
-            return False
-    
+        return self.charactere_actuelle.isalpha() or self.charactere_actuelle == '_'
+
     def fin_de_mot(self):
-        fin =[',','\n',' ','+','-',':','(',')','[',']','/','*','=','.']
-        if self.charactere_actuelle in fin:
-            return True
-        else:
-            return False
-        
+        fin = [',', '\n', ' ', '+', '-', ':', '(', ')', '[', ']', '/', '*', '=', '.']
+        return self.charactere_actuelle in fin
+    def unary_operator(self):
+        operator_type = TokenType.is_unary_operator(self.charactere_actuelle)
+        if operator_type:
+            
+            return OperatorUnaryToken(self.charactere_actuelle, self.ligne_position, self.curseur_position)
+        return None  
+
     def binary(self):
-        next_charactere = self.contenu[self.position + 1]
-        binary_token = self.charactere_actuelle + next_charactere
-        match binary_token:
-            case '&&':
-                print("Opérateur logique ET", binary_token)
-                self.lire()
-            case '||':
-                print("Opérateur logique OU", binary_token)
-                self.lire()
-            case '==':
-                print("Opérateur de comparaison égalité", binary_token)
-                self.lire()
-            case '!=':
-                print("Opérateur de comparaison différence", binary_token)
-                self.lire()
-            case '<=':
-                print("Opérateur inférieur ou égal", binary_token)
-                self.lire()
-            case '>=':
-                print("Opérateur supérieur ou égal", binary_token)
-                self.lire()
-            case '++':
-                print("Opérateur d'incrémentation", binary_token)
-                self.lire()
-            case '--':
-                print("Opérateur de décrémentation", binary_token)
-                self.lire()
-            case '//':
-                print("Division entière", binary_token)
-                self.lire()
-            case '<<':
-                print("Décalage à gauche", binary_token)
-                self.lire()
-            case '>>':
-                print("Décalage à droite", binary_token)
-                self.lire()
-            case '**':
-                print("Opérateur de puissance", binary_token)
-                self.lire()
-            case _:
-                match self.charactere_actuelle:
-                    case '-':
-                        print("soustraction",self.charactere_actuelle)
-                    case '*':
-                        print("multiplication",self.charactere_actuelle)
-                    case '+':
-                        print("plus",self.charactere_actuelle)
-                    case '/':
-                        print("divise à", self.charactere_actuelle)
-                    case '=':
-                        print("égale",self.charactere_actuelle)
-                    case '<':
-                        print("inférieur à", self.charactere_actuelle)
-                    case '>':
-                        print("supérieur à", self.charactere_actuelle)
-                    case '!':
-                        print(" l'opposé", self.charactere_actuelle)
-                    case '&':
-                        print(" ET", self.charactere_actuelle)
-                    case '|':
-                        print(" OU", self.charactere_actuelle)
-                    case '"':
-                        print('"', self.charactere_actuelle)
+      
+        if self.curseur_position + 1 < len(self.contenu):
+            next_charactere = self.contenu[self.curseur_position]
+            binary_token = self.charactere_actuelle + next_charactere
+
+            operator_type = TokenType.is_binary_operator(binary_token)
+            if operator_type:
+                
+                self.lire()  
+                return OperatorBinaryToken(binary_token, self.ligne_position, self.curseur_position)
+            else:
+                return self.unary_operator()
+        else:
+            return self.unary_operator()
 
 
     def mot_cle(self):
-        match self.token:
-            case 'if':
-                print("condition if")
-            case 'elif':
-                print("condition elif")
-            case 'else':
-                print("condition else")
-            case 'and':
-                print("condition ET")
-            case 'or':
-                print("condition OU")
-            case 'for':
-                print("boucle for")
-            case 'while':
-                print("boucle while")
-            case 'def':
-                print("définition de fonction")
-            case 'class':
-                print("définition de classe")
-            case 'import':
-                print("importation de module")
-            case 'from':
-                print("importation de parties de module")
-            case 'try':
-                print("blocs try-except")
-            case 'except':
-                print("gestion d'exception")
-            case 'finally':
-                print("bloc finally")
-            case 'with':
-                print("utilisation du gestionnaire de contexte")
-            case 'return':
-                print("retourner une valeur")
-            case 'break':
-                print("sortir de la boucle")
-            case 'continue':
-                print("continuer à l'itération suivante")
-            case 'pass':
-                print("ne rien faire")
-            case 'True':
-                print("la condidtion est vrai")
-            case 'False':
-                print("la condidtion est fause")
-            case 'print':
-                print("on affiche")
-            case _:
-                if(self.token_nombre == False):
-                    print("variable",self.token)
-                else:
-                    print("nombre",self.token)
+        keywords = {
+            'if': 'IF', 'elif': 'ELIF', 'else': 'ELSE', 'for': 'FOR', 'while': 'WHILE',
+            'def': 'DEF', 'return': 'RETURN', 'import': 'IMPORT', 'from': 'FROM'
+        }
+
+        if self.token in keywords:
+            return KeywordToken(self.token, self.ligne_position, self.curseur_position)
+        elif self.token_nombre:
+            return LiteralToken(self.token, self.ligne_position, self.curseur_position)
+        else:
+            return IdentifierToken(self.token, self.ligne_position, self.curseur_position)
 
     def Tokenisation(self):
-        
-        if((self.charactere() or  (self.charactere_actuelle == '_') ) &  (self.token == None)):
-            self.token = self.charactere_actuelle    
-        elif(self.token != None):
-            if(self.fin_de_mot()):
-                self.mot_cle()
-                self.token = None
-                self.token_nombre = False 
-                self.Tokenisation()
-            else:
-                self.token += self.charactere_actuelle               
-        elif((self.chiffre()) & (self.token == None)):
-            if(self.charactere_actuelle == '0'):
-                print("nombre",self.charactere_actuelle)
-            else:
-                self.token = self.charactere_actuelle
-                self.token_nombre = True        
-        else:
-            match self.charactere_actuelle:
-                case '#':
-                    while self.charactere_actuelle!='\n':
-                        self.lire()
-                case ',':
-                    print("virgule",self.charactere_actuelle)
-                case ':':
-                    print("fin de la definion d'une boucle",self.charactere_actuelle)
-                case '%':
-                    print("reste de la division euclidienne",self.charactere_actuelle)
-                case '\n':
-                    print("saut de ligne",'\\n')
-                    indent=0
-                    self.lire()
-                    while self.charactere_actuelle == ' ':
-                        indent+=1
-                        self.lire()
-                    self.retour()
-                    head = self.pile[0]
-                    if head==indent:
-                        pass
-                    elif head<indent:
-                        self.pile = [indent]+self.pile
-                        head=self.pile[0]
-                        print("BEGIN")
-                    else:
-                        while head>indent:
-                            print("END")
-                            self.pile = self.pile[1:]
-                            head = self.pile[0]
-                            if (head<indent):
-                                return "indentation error"         
-                case '(':
-                    print("parenthèse ouvrante ",self.charactere_actuelle)
-                case ')':
-                    print("parenthèse fermante ",self.charactere_actuelle)
-                case ' ':
-                    pass
-                case '-':
-                    self.binary()
-                case '*':
-                    self.binary()
-                case '+':
-                    self.binary()
-                case '/':
-                    self.binary()
-                case '=':
-                    self.binary()
-                case '<':
-                    self.binary()
-                case '>':
-                    self.binary()
-                case '!':
-                    self.binary()
-                case '&':
-                    self.binary()
-                case '|':
-                    self.binary()
-                case _:
-                  print("illégale",self.charactere_actuelle)
-    
+        tokens = []
 
+        while not self.fin_fichier:
+            self.lire()
+
+            
+            if self.charactere() or self.charactere_actuelle == '_':
+                if self.token is None:
+                    self.token = self.charactere_actuelle
+                elif not self.fin_de_mot():
+                    self.token += self.charactere_actuelle
+                else:
+                    
+                    tokens.append(self.mot_cle())
+                    self.token = None
+
+            
+            elif self.chiffre():
+                if not self.token:
+                    self.token = self.charactere_actuelle
+                    self.token_nombre = True
+                elif self.token_nombre:
+                    self.token += self.charactere_actuelle
+                else:
+                    tokens.append(self.mot_cle())
+                    self.token = self.charactere_actuelle
+                    self.token_nombre = True
+
+            
+            elif self.charactere_actuelle == '\n':
+                if self.token:
+                    tokens.append(self.mot_cle())
+                    self.token = None
+                    self.token_nombre = False
+                tokens.append(NewlineToken(self.ligne_position, self.curseur_position))
+
+            
+            elif self.charactere_actuelle in '(){}[]':
+                tokens.append(PunctuationToken(self.charactere_actuelle, self.ligne_position, self.curseur_position))
+
+            
+            else:
+                if self.token:
+                    tokens.append(self.mot_cle())
+                    self.token = None
+                    self.token_nombre = False
+
+                
+                operator_token = self.binary()  
+                if operator_token:
+                    tokens.append(operator_token)
+
+        # Ajouter le token EOF à la fin
+        tokens.append(BaseToken(TokenType.EOF, '', self.ligne_position, self.curseur_position))
+        return tokens
+
+
+  
--- Code/analyse_lexicale/token.py
+++ Code/analyse_lexicale/token.py
@@ -1,174 +1,98 @@
-# Ce script servira à introduire et définir les différents Tokens dans lee cadre de l'analyse lexicale 
+from enum import Enum
 
-from enum import auto, Enum
 
 class TokenType(Enum):
-    # Mots-clés
-    IF = auto()  # 'if'
-    ELIF = auto()  # 'elif'
-    ELSE = auto()  # 'else'
-    FOR = auto()  # 'for'
-    WHILE = auto()  # 'while'
-    DEF = auto()  # 'def'
-    CLASS = auto()  # 'class'
-    IMPORT = auto()  # 'import'
-    FROM = auto()  # 'from'
-    RETURN = auto()  # 'return'
-    BREAK = auto()  # 'break'
-    CONTINUE = auto()  # 'continue'
-    PASS = auto()  # 'pass'
-    TRUE = auto()  # 'True'
-    FALSE = auto()  # 'False'
-    
-    # Opérateurs
-    PLUS = auto()  # '+'
-    MINUS = auto()  # '-'
-    MULTIPLY = auto()  # '*'
-    DIVIDE = auto()  # '/'
-    MODULO = auto()  # '%'
-    POWER = auto()  # '**'
-    INCREMENT = auto()  # '++'
-    DECREMENT = auto()  # '--'
-    
-    # Opérateurs de comparaison
-    EQUAL = auto()  # '=='
-    NOT_EQUAL = auto()  # '!='
-    LESS_THAN = auto()  # '<'
-    GREATER_THAN = auto()  # '>'
-    LESS_EQUAL = auto()  # '<='
-    GREATER_EQUAL = auto()  # '>='
-    
-    # Opérateurs logiques
-    AND = auto()  # '&&'
-    OR = auto()  # '||'
-    NOT = auto()  # '!'
-    
-    # Autres symboles
-    ASSIGN = auto()  # '='
-    OPEN_PAREN = auto()  # '('
-    CLOSE_PAREN = auto()  # ')'
-    COMMA = auto()  # ','
-    COLON = auto()  # ':'
-    NEWLINE = auto()  # '\n'
-    INDENT = auto()  # indentation
-    DEDENT = auto()  # dé-dent
-    
-    # Types de données
-    IDENTIFIER = auto()  # Variable ou nom d'identifiant
-    NUMBER = auto()  # Un nombre (entier ou flottant)
-    STRING = auto()  # Une chaîne de caractères
-    
-    # Fin du fichier
-    EOF = auto()  # End Of File
+    IDENTIFIER = 'IDENTIFIER'
+    NUMBER = 'NUMBER'
+    KEYWORD = 'KEYWORD'
+    OPERATOR_UNARY = 'OPERATOR_UNARY'
+    OPERATOR_BINARY = 'OPERATOR_BINARY'
+    PUNCTUATION = 'PUNCTUATION'
+    INDENT = 'INDENT'
+    DEDENT = 'DEDENT'
+    NEWLINE = 'NEWLINE'
+    EOF = 'EOF'
 
-    # Classe de base pour un Token
+    
+    @classmethod
+    def is_binary_operator(cls, token):
+        binaires = {
+            '&&': 'LOGICAL_AND',
+            '||': 'LOGICAL_OR',
+            '==': 'EQUAL',
+            '!=': 'NOT_EQUAL',
+            '<=': 'LESS_EQUAL',
+            '>=': 'GREATER_EQUAL',
+            '++': 'INCREMENT',
+            '--': 'DECREMENT',
+            '//': 'FLOOR_DIV',
+            '<<': 'SHIFT_LEFT',
+            '>>': 'SHIFT_RIGHT',
+            '**': 'POWER'
+        }
+        return binaires.get(token, None)
+
+    
+    @classmethod
+    def is_unary_operator(cls, token):
+        unaires = {
+            '+': 'PLUS',
+            '-': 'MINUS',
+            '!': 'NOT',
+            '~': 'BITWISE_NOT'
+        }
+        return unaires.get(token, None)
+
+
+
 class BaseToken:
-    def __init__(self, token_type: TokenType, value: str, ligne: int, colonne: int):
-        self.token_type = token_type  # Type du token (ex. KEYWORD, OPERATOR)
-        self.value = value  # Valeur du token (texte du token)
-        self.ligne = ligne  # Ligne où le token a été trouvé
-        self.colonne = colonne  # Colonne où le token a été trouvé
+    def __init__(self, type_, value, line, column):
+        self.type = type_
+        self.value = value
+        self.line = line
+        self.column = column
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(Type: {self.token_type.name}, Value: {repr(self.value)}, Ligne: {self.ligne}, Colonne: {self.colonne})"
+        return f'{self.type.name}({self.value}) at line {self.line}, column {self.column}'
 
 
-# Classe pour les mots-clés (ex. if, else, for)
 class KeywordToken(BaseToken):
-    def __init__(self, value: str, ligne: int, colonne: int):
-        super().__init__(TokenType.KEYWORD, value, ligne, colonne)
+    def __init__(self, value, line, column):
+        super().__init__(TokenType.KEYWORD, value, line, column)
 
-
-# Classe pour les opérateurs (ex. +, -, ==)
 class OperatorToken(BaseToken):
-    def __init__(self, value: str, ligne: int, colonne: int):
-        super().__init__(TokenType.OPERATOR, value, ligne, colonne)
+    def __init__(self, value, line, column):
+        super().__init__(TokenType.OPERATOR, value, line, column)
 
-
-# Classe pour les littéraux comme les nombres ou chaînes de caractères
 class LiteralToken(BaseToken):
-    def __init__(self, value: str, ligne: int, colonne: int):
-        super().__init__(TokenType.LITERAL, value, ligne, colonne)
+    def __init__(self, value, line, column):
+        super().__init__(TokenType.NUMBER, value, line, column)
 
-
-# Classe pour les identifiants (ex. noms de variables, fonctions)
 class IdentifierToken(BaseToken):
-    def __init__(self, value: str, ligne: int, colonne: int):
-        super().__init__(TokenType.IDENTIFIER, value, ligne, colonne)
+    def __init__(self, value, line, column):
+        super().__init__(TokenType.IDENTIFIER, value, line, column)
 
-
-# Classe pour la ponctuation (ex. , ; ( ))
 class PunctuationToken(BaseToken):
-    def __init__(self, value: str, ligne: int, colonne: int):
-        super().__init__(TokenType.PUNCTUATION, value, ligne, colonne)
+    def __init__(self, value, line, column):
+        super().__init__(TokenType.PUNCTUATION, value, line, column)
 
+class NewlineToken(BaseToken):
+    def __init__(self, line, column):
+        super().__init__(TokenType.NEWLINE, '\\n', line, column)
 
-# Classe pour les tokens spéciaux (ex. saut de ligne, indentation, etc.)
-class SpecialToken(BaseToken):
-    def __init__(self, value: str, ligne: int, colonne: int):
-        super().__init__(TokenType.SPECIAL, value, ligne, colonne)
+class IndentToken(BaseToken):
+    def __init__(self, line, column):
+        super().__init__(TokenType.INDENT, 'INDENT', line, column)
 
-# Exemple de classes dérivées de SpecialToken pour des cas spécifiques
-class NewlineToken(SpecialToken):
-    def __init__(self, ligne: int, colonne: int):
-        super().__init__('\\n', ligne, colonne)
+class DedentToken(BaseToken):
+    def __init__(self, line, column):
+        super().__init__(TokenType.DEDENT, 'DEDENT', line, column)
 
-class IndentToken(SpecialToken):
-    def __init__(self, ligne: int, colonne: int):
-        super().__init__('INDENT', ligne, colonne)
+class OperatorUnaryToken(BaseToken):
+    def __init__(self, value, line, column):
+        super().__init__(TokenType.OPERATOR_UNARY, value, line, column)
 
-class DedentToken(SpecialToken):
-    def __init__(self, ligne: int, colonne: int):
-        super().__init__('DEDENT', ligne, colonne)
+class OperatorBinaryToken(BaseToken):
+    def __init__(self, value, line, column):
+        super().__init__(TokenType.OPERATOR_BINARY, value, line, column)
 
-# Ce fichier définit différentes classes pour représenter les tokens dans un analyseur lexical.
-# Il repose sur une hiérarchie claire de classes pour encapsuler différents types de tokens.
-
-# 1. `TokenType` est une énumération (Enum) qui liste les types génériques de tokens.
-#    Chaque type de token est associé à une catégorie (mots-clés, opérateurs, littéraux, etc.).
-#    Cette énumération utilise `auto()` pour générer automatiquement les valeurs de chaque type,
-#    ce qui permet une évolution plus facile du code sans avoir à assigner manuellement des valeurs constantes.
-
-# 2. `BaseToken` est la classe de base pour tous les tokens. Elle prend en entrée :
-#    - `token_type` : Un type issu de `TokenType`, qui détermine la catégorie du token.
-#    - `value` : La valeur réelle du token, c'est-à-dire le texte capturé par l'analyse lexicale (ex : "if", "42", "+").
-#    - `ligne` et `colonne` : Les coordonnées du token dans le texte source (ligne et colonne).
-#    Cette classe définit également une méthode `__repr__` qui formate l'affichage du token 
-#    pour le débogage ou la lecture humaine. Cela permet de visualiser rapidement de quel type est le token,
-#    quelle est sa valeur, et où il se trouve dans le fichier source.
-
-# 3. Les classes dérivées de `BaseToken` :
-#    - Chaque type de token est représenté par une sous-classe spécialisée de `BaseToken`, héritant des attributs
-#      et méthodes de `BaseToken`. Cela permet d'étendre ou de modifier facilement le comportement pour chaque type
-#      de token, si nécessaire.
-#    
-#    3.1. `KeywordToken` : Spécialise `BaseToken` pour les mots-clés du langage (comme `if`, `else`, `while`).
-#         Ce type de token est associé à `TokenType.KEYWORD` et représente les instructions du langage de programmation.
-#         Exemple : `KeywordToken("if", 1, 0)` représente le mot-clé `if` trouvé à la première ligne, première colonne.
-
-#    3.2. `OperatorToken` : Cette classe est utilisée pour les opérateurs (comme `+`, `-`, `==`).
-#         L'analyse lexicale reconnaît ces symboles comme des opérateurs et crée un objet `OperatorToken`
-#         avec `TokenType.OPERATOR`. Exemple : `OperatorToken("==", 1, 3)` pour l'opérateur `==` à la ligne 1, colonne 3.
-
-#    3.3. `LiteralToken` : Représente les littéraux, comme les nombres ou les chaînes de caractères.
-#         Les littéraux sont des valeurs directes dans le code source, comme `42` pour un entier, ou `"Hello"` pour une chaîne.
-#         Exemple : `LiteralToken("42", 1, 15)` représente le nombre `42` trouvé à la ligne 1, colonne 15.
-
-#    3.4. `IdentifierToken` : Utilisé pour les identifiants (comme les noms de variables, de fonctions ou de classes).
-#         Un identifiant est un nom donné par le programmeur pour référencer une entité dans le programme (comme une variable).
-#         Exemple : `IdentifierToken("variable", 1, 6)` représente l'identifiant `variable` à la ligne 1, colonne 6.
-
-#    3.5. `PunctuationToken` : Représente les caractères de ponctuation tels que `,`, `;`, `(`, `)`, etc.
-#         Ce type de token est utilisé pour tous les symboles qui servent à la structure du code (parenthèses, virgules, etc.).
-#         Exemple : `PunctuationToken("(", 1, 13)` représente une parenthèse ouvrante trouvée à la ligne 1, colonne 13.
-
-#    3.6. `SpecialToken` : Représente des tokens spéciaux qui ne rentrent pas dans les catégories précédentes.
-#         Cela inclut les retours à la ligne (`\n`), les indentations (`INDENT`), les dé-dentations (`DEDENT`), ou encore
-#         les commentaires. Les classes dérivées comme `NewlineToken`, `IndentToken` et `DedentToken` permettent de
-#         capturer ces événements spéciaux dans l'analyse lexicale.
-
-# 4. `__repr__` dans chaque classe dérivée :
-#    Chaque classe hérite de la méthode `__repr__` de la classe `BaseToken`, ce qui permet d'afficher
-#    une représentation lisible et utile des tokens pour les tests et le débogage.
-#    Cela produit une chaîne de caractères de la forme : `Token(Type: KEYWORD, Value: 'if', Ligne: 1, Colonne: 0)`
-#    où le type de token, sa valeur et sa position dans le fichier source sont clairement indiqués.
