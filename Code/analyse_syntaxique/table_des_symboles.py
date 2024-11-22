7a9945564a1dbe72fc192e737172095f29b708b3
MHamdi Amine <amine.mhamdi@telecomnancy.eu>
2024-11-22 21:06:52 +0100

    création de la class table des symboles

---
+++ Code/analyse_syntaxique/table_des_symboles.py
@@ -0,0 +1,9 @@
+class TDS:
+    def __init__(self, region_id):
+        self.region_id = region_id
+        self.pere = -1
+        self.fils = []
+        self.frere = []
+        self.declaration = []
+
+
