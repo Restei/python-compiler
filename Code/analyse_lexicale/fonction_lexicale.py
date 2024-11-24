b2b152db1df3aa079a8382b798b17f5616ae6c76
MHamdi Amine <amine.mhamdi@telecomnancy.eu>
2024-11-24 11:48:30 +0100

    reglage du parametre column qui n'etais pas le bon

--- Code/analyse_lexicale/fonction_lexicale.py
+++ Code/analyse_lexicale/fonction_lexicale.py
@@ -25,6 +25,7 @@ class Lexeur:
     def __init__(self, contenu):
         self.contenu = contenu + " "
         self.taille = len(self.contenu)
+        self.column = -1
         self.position = -1
         self.ligne_position = 1
         self.curseur_position = 0
@@ -45,10 +46,12 @@ class Lexeur:
             self.fin_fichier = True
         else:
             self.position += 1
+            self.column += 1
             self.curseur_position += 1
             self.charactere_actuelle = self.contenu[self.position]
             if self.charactere_actuelle == '\n':
                 self.ligne_position += 1
+                self.column = 0
 
     def retour(self):
         if self.curseur_position >= 1:
@@ -86,7 +89,7 @@ class Lexeur:
         operator_type = TokenType.is_unary_operator(self.charactere_actuelle)
         if operator_type:
             
-            return OperatorUnaryToken(self.charactere_actuelle, self.ligne_position, self.position)
+            return OperatorUnaryToken(self.charactere_actuelle, self.ligne_position, self.column)
         return None  
 
     def binary(self):
@@ -99,7 +102,7 @@ class Lexeur:
             if operator_type:
                 
                 self.lire()  
-                return OperatorBinaryToken(binary_token, self.ligne_position, self.position)
+                return OperatorBinaryToken(binary_token, self.ligne_position, self.column)
             else:
                 return self.unary_operator()
         else:
@@ -114,23 +117,23 @@ class Lexeur:
             'print':'PRINT','None':'NONE'
         }
         if self.variable_error or self.number_error:
-            return UnknownToken(self.token,self.ligne_position,self.position)
+            return UnknownToken(self.token,self.ligne_position,self.column)
         
         if self.token in keywords:
-            return KeywordToken(self.token, self.ligne_position, self.position)
+            return KeywordToken(self.token, self.ligne_position, self.column)
         
         elif self.token_nombre:
             if self.token[0]=='0':
                 if len(self.token)>1:
                     self.errors.append(ZeroException(self.ligne_position,self.token))
-                    return UnknownToken(self.token,self.ligne_position,self.position)
-            return LiteralToken(self.token, self.ligne_position, self.position)
+                    return UnknownToken(self.token,self.ligne_position,self.column)
+            return LiteralToken(self.token, self.ligne_position, self.column)
         
         elif self.string:
-            return StringToken(self.token,self.ligne_position,self.position)
+            return StringToken(self.token,self.ligne_position,self.column)
         
         else:
-            return IdentifierToken(self.token, self.ligne_position, self.position)
+            return IdentifierToken(self.token, self.ligne_position, self.column)
 
     def Identification(self,tokens):
         if self.is_indent==True and self.charactere_actuelle!='\n':
@@ -145,7 +148,7 @@ class Lexeur:
                     pass
                 elif self.count>self.pile_indent[0]:
                     self.pile_indent = [self.count] + self.pile_indent
-                    tokens.append(IndentToken(self.ligne_position,self.position))
+                    tokens.append(IndentToken(self.ligne_position,self.column ))
                     self.count = 0
                 else:
                     if self.count in self.pile_indent:
@@ -205,7 +208,7 @@ class Lexeur:
   
         
         elif self.charactere_actuelle == '\n':
-            tokens.append(NewlineToken(self.ligne_position,self.position))
+            tokens.append(NewlineToken(self.ligne_position,self.column))
             while self.peek()=='\n':
                 self.lire()
             if self.token:
@@ -218,7 +221,7 @@ class Lexeur:
 
         
         elif self.charactere_actuelle in '(){}[]:,' and not self.string:
-            tokens.append(PunctuationToken(self.charactere_actuelle, self.ligne_position, self.position))
+            tokens.append(PunctuationToken(self.charactere_actuelle, self.ligne_position, self.column))
         
         
         else:
@@ -226,7 +229,7 @@ class Lexeur:
             if operator_token:
                 tokens.append(operator_token)
             elif self.caractere_inconnu():
-                tokens.append(UnknownToken(self.charactere_actuelle,self.ligne_position,self.position))
+                tokens.append(UnknownToken(self.charactere_actuelle,self.ligne_position,self.column))
                 self.errors.append(UnknowCaracters((self.ligne_position),(self.charactere_actuelle)))
                 
     def Tokenisation(self):
@@ -236,6 +239,6 @@ class Lexeur:
             self.Identification(tokens)
         
         # Ajouter le token EOF à la fin
-        tokens.append(BaseToken(TokenType.EOF, '', self.ligne_position, self.position))
+        tokens.append(BaseToken(TokenType.EOF, '', self.ligne_position, self.column))
 
         return tokens,self.errors
--- Code/lexeur_test.py
+++ Code/lexeur_test.py
@@ -3,11 +3,17 @@ from analyse_lexicale.fonction_lexicale import Lexeur
 
 # Fonction pour tester les tokens générés par rapport aux attendus
 def test_lexer(code: str, expected: List[Tuple[str, str, int, int]]):
-    generated_tokens = Lexeur(code).Tokenisation()
+    generated_token,errors = Lexeur(code).Tokenisation()
     matched = 0
     missing_tokens = []
     extra_tokens = []
-
+    generated_tokens = []
+    
+    #for i in range(len(generated_token)):
+    #    token = generated_token[i]
+    for token in generated_token:
+        generated_tokens.append((token.type.value,token.value,token.line,token.column))
+        
     # Comparaison des tokens
     for token in expected:
         if token in generated_tokens:
@@ -28,114 +34,13 @@ def test_lexer(code: str, expected: List[Tuple[str, str, int, int]]):
     print(f"Tokens en trop : {extra_tokens}")
 
 # Exemple de code source
-code_source = """
-# Début du programme
-def calcul_complexe(a, b):
-    somme = a + b
-    produit = a * b
-    division = a / b
-    reste = a % b
-    quotient = a // b
-    puissance = a ** b
-    if a == b:
-        print("Les nombres sont égaux")
-    elif a != b:
-        print("Les nombres sont différents")
-    return somme, produit, quotient, puissance
-
-def main():
-    x = 10
-    y = 5
-    result = calcul_complexe(x, y)
-
-if __name__ == "__main__":
-    main()
-"""
+code_source = """def calcul_complexe"""
 
 # Tokens attendus (simplifié pour l'exemple)
-expected_tokens = [COMMENT(# Début du programme) at line 1, column 0,
-NEWLINE() at line 1, column 21,
-KEYWORD(def) at line 2, column 0,
-IDENTIFIER(calcul_complexe) at line 2, column 4,
-PUNCTUATION(() at line 2, column 18,
-IDENTIFIER(a) at line 2, column 19,
-PUNCTUATION(,) at line 2, column 20,
-IDENTIFIER(b) at line 2, column 22,
-PUNCTUATION()) at line 2, column 23,
-PUNCTUATION(:) at line 2, column 24,
-NEWLINE() at line 3, column 0,
-COMMENT(# Opérations arithmétiques de base) at line 3, column 4,
-NEWLINE() at line 4, column 0,
-IDENTIFIER(somme) at line 4, column 4,
-OPERATOR_UNARY(=) at line 4, column 10,
-IDENTIFIER(a) at line 4, column 12,
-OPERATOR_BINARY(+) at line 4, column 14,
-IDENTIFIER(b) at line 4, column 16,
-NEWLINE() at line 5, column 0,
-IDENTIFIER(produit) at line 5, column 4,
-OPERATOR_UNARY(=) at line 5, column 12,
-IDENTIFIER(a) at line 5, column 14,
-OPERATOR_BINARY(*) at line 5, column 16,
-IDENTIFIER(b) at line 5, column 18,
-NEWLINE() at line 6, column 0,
-IDENTIFIER(division) at line 6, column 4,
-OPERATOR_UNARY(=) at line 6, column 13,
-IDENTIFIER(a) at line 6, column 15,
-OPERATOR_BINARY(/) at line 6, column 17,
-IDENTIFIER(b) at line 6, column 19,
-NEWLINE() at line 7, column 0,
-IDENTIFIER(reste) at line 7, column 4,
-OPERATOR_UNARY(=) at line 7, column 10,
-IDENTIFIER(a) at line 7, column 12,
-OPERATOR_BINARY(%) at line 7, column 14,
-IDENTIFIER(b) at line 7, column 16,
-NEWLINE() at line 8, column 0,
-IDENTIFIER(quotient) at line 8, column 4,
-OPERATOR_UNARY(=) at line 8, column 13,
-IDENTIFIER(a) at line 8, column 15,
-OPERATOR_BINARY(//) at line 8, column 17,
-IDENTIFIER(b) at line 8, column 19,
-NEWLINE() at line 9, column 0,
-IDENTIFIER(puissance) at line 9, column 4,
-OPERATOR_UNARY(=) at line 9, column 14,
-IDENTIFIER(a) at line 9, column 16,
-OPERATOR_BINARY(**) at line 9, column 18,
-IDENTIFIER(b) at line 9, column 20,
-NEWLINE() at line 10, column 0,
-COMMENT(# Opérations logiques et comparaisons) at line 10, column 4,
-NEWLINE() at line 11, column 0,
-KEYWORD(if) at line 11, column 4,
-IDENTIFIER(a) at line 11, column 7,
-OPERATOR_BINARY(==) at line 11, column 9,
-IDENTIFIER(b) at line 11, column 12,
-PUNCTUATION(:) at line 11, column 13,
-NEWLINE() at line 12, column 0,
-BEGIN() at line 12, column 4,
-KEYWORD(print) at line 12, column 8,
-PUNCTUATION(() at line 12, column 13,
-STRING("Les nombres sont égaux") at line 12, column 14,
-PUNCTUATION()) at line 12, column 38,
-NEWLINE() at line 13, column 0,
-END() at line 13, column 4,
-KEYWORD(elif) at line 13, column 8,
-IDENTIFIER(a) at line 13, column 13,
-OPERATOR_BINARY(!=) at line 13, column 15,
-IDENTIFIER(b) at line 13, column 18,
-PUNCTUATION(:) at line 13, column 19,
-NEWLINE() at line 14, column 0,
-BEGIN() at line 14, column 4,
-KEYWORD(print) at line 14, column 8,
-PUNCTUATION(() at line 14, column 13,
-STRING("Les nombres sont différents") at line 14, column 14,
-PUNCTUATION()) at line 14, column 39,
-NEWLINE() at line 15, column 0,
-END() at line 15, column 4,
-COMMENT(\# Comparaisons binaires) at line 16, column 4,
-NEWLINE() at line 17, column 0,
-KEYWORD(if) at line 17, column 4,
-IDENTIFIER(a) at line 17, column 7,
-OPERATOR_BINARY(>=) at line 17, column 9,
-IDENTIFIER(b) at line 17, column 12,
+expected_tokens = [
+    ("KEYWORD","def",1,3),
+    ("IDENTIFIER","calcul_complexe",1,19),
+    ('EOF', '', 1, 19)
 ]
 
 # Exécuter le test
