1b15c211a1c79729d4fcd92dd03eda3d8f2b0aa9
Russel Tchiakaka <russel.tchiakaka@telecomnancy.eu>
2024-12-06 18:59:08 +0100

    Oublie main.py

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from analyse_lexicale.fonction_lexicale import Lexeur,lire_fichier,affichage_fichier
 from analyse_lexicale.token import TokenType, BaseToken
 from analyse_syntaxique.table_des_symboles import *
@@ -6,42 +5,17 @@ from analyse_syntaxique.fonction_syntaxique_dictionnaire import *
 
 path = "mini_python/"
 fichier = "test1.py"
-=======
-from analyse_syntaxique.fonction_syntaxique import * 
-from analyse_syntaxique.GrammairesLL1 import *
 
-# Charger le contenu du fichier source
-fichier_source = lire_fichier("Code/mini_python/expression.py")
->>>>>>> df908c0456185923192398672dadc3fca0fcb5f4
+file = path + fichier
 
-# Étape 1 : Analyse lexicale
-Lex1 = Lexeur(fichier_source)  # Instancie le lexeur
-Lex1.Tokenisation()  # Génère les tokens et détecte les erreurs lexicales
+Lex1 = Lexeur(lire_fichier(file))
 
-if Lex1.errors:
-    # Si des erreurs lexicales sont détectées, les afficher
-    print("Erreurs lexicales détectées :")
-    for erreur in Lex1.errors:
-        print(erreur)
-else:
-    print("Analyse lexicale réussie. Les tokens sont prêts pour l'analyse syntaxique.")
+#print(lire_fichier("mini_python/variable.py"))
 
-# Étape 2 : Analyse syntaxique
-try:
-    parser = LL1Parser(fichier_source)  # Instancie le parseur LL(1)
-    parser.parse()  # Lance l'analyse syntaxique
+Tokens,errors = Lex1.Tokenisation()
+#affichage_fichier(file)
+print("\n")
 
-    if parser.errors:
-        # Si des erreurs syntaxiques sont détectées, les afficher
-        print("\nErreurs syntaxiques détectées :")
-        for erreur in parser.errors:
-            print(erreur)
-    else:
-        print("\nAnalyse syntaxique réussie. Aucun problème détecté.")
-except Exception as e:
-    print(f"Une erreur critique est survenue : {e}")
-
-<<<<<<< HEAD
 for token in Tokens:
     print(repr(token))
 #    
@@ -65,6 +39,4 @@ for token in Tokens:
 #except Exception as e:
 #    print(f"Une erreur critique est survenue : {e}")
 
-parse_with_tokens(ll1_table,Tokens,"file")
-=======
->>>>>>> df908c0456185923192398672dadc3fca0fcb5f4
+parse_with_tokens(grammar,LL2_entries,Tokens,"file")
