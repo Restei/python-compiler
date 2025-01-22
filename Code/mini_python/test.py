bc158ba7cbf5a41283fb0ed0b47b90f78bbcb71b
lina <lina@LAPTOP-GA52FUVE.localdomain>
2025-01-22 10:36:31 +0100

    clean tests

--- Code/mini_python/test.py
+++ Code/mini_python/test.py
@@ -1,37 +1,24 @@
 def operation(a, b, c):
-    # Opérations simples , associativité et priorité des opérateurs
+
     somme = a + b - c
     produit = a + b * c
-    div = a / (b + 1)
-    
-    # Comparaison et condition
+    div = a // (b + 1)
+
     if a > b:
         print("a > b")
-    elif a == c:
-        print("a == c")
     else:
-        
         print("a < b and a != c") 
-        
 
-    # Boucle
     for i in range(a):
-        prin(i) # Erreur syntaxique : print mal orthographié
-
-    # Liste
+        print(i)
     lst = [a, b, c]
-    
-
-    # Retour des résultats
-    return somme, produit, div, lst
+    lst[1]=2
+    return somme 
 
 def main():
-    x = 5
+    x = 5+5+5+5+5
     y = 3
     z = 7
 
     result = operation(x, y, z)
     print("Resultats:", result)
-
-if __name__ == "__main__":
-    main()
